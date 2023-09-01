00f60d79e6d2de7610ee98cbd479c75faefbf328
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-08-31 18:26:00 -0600

    Avance 31/08/2023

---
+++ procesos/actualizar_Flujo.php
@@ -0,0 +1,39 @@
+<?php
+    include "../clases/Conexion.php";
+    include "../clases/crud.php";
+
+    $Crud = new Crud();
+    $id = $_POST['id'];
+
+    $datos = array(
+        "CREAR_RECIBO" => $_POST['crear'], 
+        "NOTAS" => $_POST['nota'],
+        "ENVIADO" => $_POST['enviado'],
+        "LLAVE" => $_POST['llave'],
+        "BENEFICIARIO" => $_POST['beneficiario'], 
+        "FACTURA" => $_POST['factura'],
+        "FECHA" => $_POST['fecha'],
+        "MOTIVO" => $_POST['motivo'],
+        "CATEGORIA" => $_POST['categoria'], 
+        "ORIGEN" => $_POST['origen'],
+        "SUBCATEGORIA" => $_POST['subcategoria'],
+        "MES" => $_POST['mes'],
+        "AÑO" => $_POST['año'], 
+        "FLUJO2" => $_POST['flujo2'],
+        "LOTE" => $_POST['lote'],
+        "CONDOMINIO" => $_POST['condominio'],
+        "CLUSTER" => $_POST['cluster'],
+        "DESARROLLO" => $_POST['desarrollo'],
+        "CLIENTE" => $_POST['cliente'],
+        "CORREO" => $_POST['correo'],
+        "ER_MENSUALIDAD" => $_POST['er_mensualidad']
+    );
+
+    $respuesta = $Crud -> actualizarFlujo($id, $datos);
+
+    if ($respuesta->getModifiedCount() > 0 || $respuesta->getMatchedCount() > 0) {
+        header("location:../flujo.php");
+    }else {
+        print_r($respuesta);
+    }
+?>
