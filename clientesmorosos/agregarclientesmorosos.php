fa8beb0818728387668c6a65599205f6d6567817
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-01 12:50:59 -0600

    Avance 01/09/2023

---
+++ clientesmorosos/agregarclientesmorosos.php
@@ -0,0 +1,76 @@
+<div class="card">
+  <div class="card-body">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <div class= "card mt-4">
+    <div class= "card-body">
+        <h2> Agregar nuevo registro  </h2>
+        <form action="./insertarclientesmorosos.php" method="post">
+            <label for="CONTADOR">CONTADOR</label>
+            <input type="text" class="form-control" id="CONTADOR" name="CONTADOR">
+            <label for="LLAVE">LLAVE</label>
+            <input type="text" class="form-control" id="LLAVE" name="LLAVE">
+            <label for="DESARROLLO">DESARROLLO</label>
+            <input type="text" class="form-control" id="DESARROLLO" name="DESARROLLO">
+            <label for="LOTE">LOTE</label>
+            <input type="text" class="form-control" id="LOTE" name="LOTE">
+            <label for="CONDOMINIO">CONDOMINIO</label>
+            <input type="text" class="form-control" id="CONDOMINIO" name="CONDOMINIO">
+            <label for="CLÚSTER">CLÚSTER</label>
+            <input type="text" class="form-control" id="CLÚSTER" name="CLÚSTER">
+            <label for="FECHA">FECHA</label>
+            <input type="text" class="form-control" id="FECHA" name="FECHA">
+            <label for="CLIENTE">CLIENTE</label>
+            <input type="text" class="form-control" id="CLIENTE" name="CLIENTE">
+            <label for="PRECIO_DEL_LOTE">PRECIO DEL LOTE</label>
+            <input type="text" class="form-control" id="PRECIO_DEL_LOTE" name="PRECIO_DEL_LOTE">
+            <label for="MONTO_MENS">MONTO MENSUAL</label>
+            <input type="text" class="form-control" id="MONTO_MENS" name="MONTO_MENS">
+            <label for="MENSUALIDAD">MENSUALIDAD</label>
+            <input type="text" class="form-control" id="MENSUALIDAD" name="MENSUALIDAD">
+            <label for="INTERES">INTERES</label>
+            <input type="text" class="form-control" id="INTERES" name="INTERES">
+            <label for="TIPO_DE_INTES">TIPO DE INTERES</label>
+            <input type="text" class="form-control" id="TIPO_DE_INTES" name="TIPO_DE_INTES">
+            <label for="I_NEGOCIADO">INTERES NEGOCIADO</label>
+            <input type="text" class="form-control" id="I_NEGOCIADO" name="I_NEGOCIADO">
+            <label for="REAL">REAL</label>
+            <input type="text" class="form-control" id="REAL" name="REAL">
+            <label for="MENS_PAGADA">MENSUALIDAD PAGADA</label>
+            <input type="text" class="form-control" id="MENS_PAGADA" name="MENS_PAGADA">
+            <label for="MEN_ADEUDADA">MENSUALIDAD ADEUDADA</label>
+            <input type="text" class="form-control" id="MEN_ADEUDADA" name="MEN_ADEUDADA">
+            <label for="FECHA_DE_ACUERDO">FECHA DE ACUERDO</label>
+            <input type="text" class="form-control" id="FECHA_DE_ACUERDO" name="FECHA_DE_ACUERDO">
+            <label for="MEDIO">MEDIO</label>
+            <input type="text" class="form-control" id="MEDIO" name="MEDIO">
+            <label for="PLAZOS">PLAZOS</label>
+            <input type="text" class="form-control" id="PLAZOS" name="PLAZOS">
+            <label for="MONTO_POR_MES">MONTO POR MES</label>
+            <input type="text" class="form-control" id="MONTO_POR_MES" name="MONTO_POR_MES">
+            <label for="INICIO">INICIO</label>
+            <input type="text" class="form-control" id="INICIO" name="INICIO">
+            <label for="FIN">FIN</label>
+            <input type="text" class="form-control" id="FIN" name="FIN">
+            <label for="ESTATUS_DE_INTERES">ESTADOS DE INTERES</label>
+            <input type="text" class="form-control" id="ESTATUS_DE_INTERES" name="ESTATUS_DE_INTERES">
+            <label for="COMENTARIO">COMENTARIO</label>
+            <input type="text" class="form-control" id="COMENTARIO" name="COMENTARIO">
+            <label for="NEGOCIADO">NEGOCIADO</label>
+            <input type="text" class="form-control" id="NEGOCIADO" name="NEGOCIADO">
+            <label for="DEPARTAMENTO">DEPARTAMENTO</label>
+            <input type="text" class="form-control" id="DEPARTAMENTO" name="DEPARTAMENTO">
+            <label for="AÑO">AÑO</label>
+            <input type="text" class="form-control" id="AÑO" name="AÑO">
+            
+           <button class="bt btn-primary mt-3">
+                      Agregar
+           </button>
+                      
+        </form>
+        </div>
+        </div>
+        </div>
+  </div>
+</div>
---
+++ clientesmorosos/insertarclientesmorosos.php
@@ -0,0 +1,48 @@
+<?php 
+    include "./../clases/Conexion.php";
+    include "./../clientesmorosos/crudclientesmorosos.php";
+
+    $crudclientesmorosos = new crudclientesmorosos();
+
+    $datos = array(
+      "CONTADOR" => $_POST['CONTADOR'], 
+      "LLAVE" => $_POST['LLAVE'],
+      "DESARROLLO" => $_POST['DESARROLLO'],
+      "LOTE" => $_POST['LOTE'],
+      "CONDOMINIO" => $_POST['CONDOMINIO'], 
+      "CLÚSTER" => $_POST['CLÚSTER'],
+      "FECHA" => $_POST['FECHA'],
+      "CLIENTE" => $_POST['CLIENTE'],
+      "PRECIO_DEL_LOTE" => $_POST['PRECIO_DEL_LOTE'], 
+      "MONTO_MENS" => $_POST['MONTO_MENS'],
+      "MENSUALIDAD" => $_POST['MENSUALIDAD'],
+      "INTERES" => $_POST['INTERES'],
+      "TIPO_DE_INTES" => $_POST['TIPO_DE_INTES'], 
+      "I_NEGOCIADO" => $_POST['I_NEGOCIADO'],
+      "REAL" => $_POST['REAL'],
+      "MENS_PAGADA" => $_POST['MENS_PAGADA'],
+      "MEN_ADEUDADA" => $_POST['MEN_ADEUDADA'],
+      "FECHA_DE_ACUERDO" => $_POST['FECHA_DE_ACUERDO'],
+      "MEDIO" => $_POST['MEDIO'],
+      "PLAZOS" => $_POST['PLAZOS'],
+      "MONTO_POR_MES" => $_POST['MONTO_POR_MES'],
+      "INICIO" => $_POST['INICIO'],
+      "FIN" => $_POST['FIN'],
+      "ESTATUS_DE_INTERES" => $_POST['ESTATUS_DE_INTERES'],
+      "COMENTARIO" => $_POST['COMENTARIO'],
+      "NEGOCIADO" => $_POST['NEGOCIADO'],
+      "DEPARTAMENTO" => $_POST['DEPARTAMENTO'],
+      "AÑO" => $_POST['AÑO'],
+      
+    );
+
+   $respuesta = $crudclientesmorosos->insertarDatos($datos);
+
+    //if ($respuesta->getInsertedId() > 0) {
+      //  header("location:../insertaracumuladosherpa.php");
+   // } else {
+     //   print_r($respuesta);
+   //}
+    
+
+?>
