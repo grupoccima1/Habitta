9a4f8d7fae9a791ac258bfb6d1643c8e90f00fa1
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-01 12:53:48 -0600

    Avance 01/09/2023

---
+++ DevolucionesClientes/agregardevolucionesclientes.php
@@ -0,0 +1,39 @@
+<div class="card">
+  <div class="card-body">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <div class= "card mt-4">
+    <div class= "card-body">
+        <h2> Agregar nuevo registro  </h2>
+        <form action="./insertardevolucionesclientes.php" method="post">
+            <label for="No">NO</label>
+            <input type="text" class="form-control" id="No" name="No">
+            <label for="FECHA">FECHA</label>
+            <input type="text" class="form-control" id="FECHA" name="FECHA">
+            <label for="CONDOMINIO">CONDOMINIO</label>
+            <input type="text" class="form-control" id="CONDOMINIO" name="CONDOMINIO">
+            <label for="LLAVE">LLAVE</label>
+            <input type="text" class="form-control" id="LLAVE" name="LLAVE">
+            <label for="CLIENTE">CLIENTE</label>
+            <input type="text" class="form-control" id="CLIENTE" name="CLIENTE">
+            <label for="MONTO">MONTO</label>
+            <input type="text" class="form-control" id="MONTO" name="MONTO">
+            <label for="BANCO">BANCO</label>
+            <input type="text" class="form-control" id="BANCO" name="BANCO">
+            <label for="MOTIVO">MOTIVO</label>
+            <input type="text" class="form-control" id="MOTIVO" name="MOTIVO">
+            <label for="MONTO_DE_LA_OPERACION">MONTO DE LA OPERACION</label>
+            <input type="text" class="form-control" id="MONTO_DE_LA_OPERACION" name="MONTO_DE_LA_OPERACION">
+            <label for="DESARROLLO">DESARROLLO</label>
+            <input type="text" class="form-control" id="DESARROLLO" name="DESARROLLO">
+            <button class="bt btn-primary mt-3">
+                      Agregar
+           </button>
+                      
+        </form>
+        </div>
+        </div>
+        </div>
+  </div>
+</div>
---
+++ DevolucionesClientes/insertardevolucionesclientes.php
@@ -0,0 +1,24 @@
+<?php 
+    include "./../clases/Conexion.php";
+    include "./../DevolucionesClientes/cruddevolucionesclientes.php";
+
+    $cruddevolucionesclientes = new cruddevolucionesclientes();
+
+    $datos = array(
+        "No" => $_POST['No'], 
+        "FECHA" => $_POST['FECHA'],
+        "CONDOMINIO" => $_POST['CONDOMINIO'],
+        "LLAVE" => $_POST['LLAVE'],
+        "CLIENTE" => $_POST['CLIENTE'], 
+        "MONTO" => $_POST['MONTO'],
+        "BANCO" => $_POST['BANCO'],
+        "MOTIVO" => $_POST['MOTIVO'],
+        "MONTO_DE_LA_OPERACION" => $_POST['MONTO_DE_LA_OPERACION'],
+        "DESARROLLO" => $_POST['DESARROLLO'], 
+        
+       
+        
+    );
+
+   $respuesta = $cruddevolucionesclientes->insertarDatos($datos);
+   ? 
