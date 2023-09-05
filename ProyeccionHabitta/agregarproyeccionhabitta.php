4678200f652298bdee5906d2f6afd7b31c6b68b2
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-04 18:00:40 -0600

    Avance 04/09/2023

---
+++ ProyeccionHabitta/agregarproyeccionhabitta.php
@@ -0,0 +1,110 @@
+<div class="card">
+  <div class="card-body">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <div class= "card mt-4">
+    <div class= "card-body">
+        <h2> Agregar nuevo registro  </h2>
+        <form action="./insertarproyeccionhabitta.php" method="post">
+            <label for="MES">MES</label>
+            <input type="text" class="form-control" id="MES" name="MES">
+            <label for="fecha_2020">fecha_2020</label>
+            <input type="text" class="form-control" id="fecha_2020" name="fecha_2020">
+            <label for="fecha_2021">fecha_2021</label>
+            <input type="text" class="form-control" id="fecha_2021" name="fecha_2021">
+            <label for="fecha_2022">fecha_2022</label>
+            <input type="text" class="form-control" id="fecha_2022" name="fecha_2022">
+            <label for="fecha_2023">fecha_2023</label>
+            <input type="text" class="form-control" id="fecha_2023" name="fecha_2023">
+            <label for="fecha_2024">fecha_2024</label>
+            <input type="text" class="form-control" id="fecha_2024" name="fecha_2024">
+            <label for="fecha_2025">fecha_2025</label>
+            <input type="text" class="form-control" id="fecha_2025" name="fecha_2025">
+            <label for="fecha_2026">fecha_2026</label>
+            <input type="text" class="form-control" id="fecha_2026" name="fecha_2026">
+            <label for="fecha_2027">fecha_2027</label>
+            <input type="text" class="form-control" id="fecha_2027" name="fecha_2027">
+            <label for="fecha_2028">fecha_2028</label>
+            <input type="text" class="form-control" id="fecha_2028" name="fecha_2028">
+            <label for="fecha_2029">fecha_2029</label>
+            <input type="text" class="form-control" id="fecha_2029" name="fecha_2029">
+            <label for="fecha_2030">fecha_2030</label>
+            <input type="text" class="form-control" id="fecha_2030" name="fecha_2030">
+            <label for="fecha_2031">fecha_2031</label>
+            <input type="text" class="form-control" id="fecha_2031" name="fecha_2031">
+            <label for="fecha_2032">fecha_2032</label>
+            <input type="text" class="form-control" id="fecha_2032" name="fecha_2032">
+            <label for="fecha_2033">fecha_2033</label>
+            <input type="text" class="form-control" id="fecha_2033" name="fecha_2033">
+            <label for="fecha_2034">fecha_2034</label>
+            <input type="text" class="form-control" id="fecha_2034" name="fecha_2034">
+            <label for="fecha_2035">fecha_2035</label>
+            <input type="text" class="form-control" id="fecha_2035" name="fecha_2035">
+            <label for="fecha_2036">fecha_2036</label>
+            <input type="text" class="form-control" id="fecha_2036" name="fecha_2036">
+            <label for="fecha_2037">fecha_2037</label>
+            <input type="text" class="form-control" id="fecha_2037" name="fecha_2037">
+            <label for="fecha_general">fecha_general</label>
+            <input type="text" class="form-control" id="fecha_general" name="fecha_general">
+            <label for="fecha_general">fecha_general</label>
+            <input type="text" class="form-control" id="fecha_general" name="fecha_general">
+            <label for="DESIERTO">DESIERTO</label>
+            <input type="text" class="form-control" id="DESIERTO" name="DESIERTO">
+            <label for="ESTEPA">ESTEPA</label>
+            <input type="text" class="form-control" id="ESTEPA" name="ESTEPA">
+            <label for="PARAMO">PARAMO</label>
+            <input type="text" class="form-control" id="PARAMO" name="PARAMO">
+            <label for="TAIGA">TAIGA</label>
+            <input type="text" class="form-control" id="TAIGA" name="TAIGA">
+            <label for="BOSQUE">BOSQUE</label>
+            <input type="text" class="form-control" id="BOSQUE" name="BOSQUE">
+            <label for="LOMAS">LOMAS</label>
+            <input type="text" class="form-control" id="LOMAS" name="LOMAS">
+            <label for="Total_general">Total_general</label>
+            <input type="text" class="form-control" id="Total_general" name="Total_general">
+            <label for="SELVA">SELVA</label>
+            <input type="text" class="form-control" id="SELVA" name="SELVA">
+            <label for="LAGO">LAGO</label>
+            <input type="text" class="form-control" id="LAGO" name="LAGO">
+            <label for="MANGLAR">MANGLAR</label>
+            <input type="text" class="form-control" id="MANGLAR" name="MANGLAR">
+            <label for="Fecha_2020">Fecha_2020</label>
+            <input type="text" class="form-control" id="Fecha_2020" name="Fecha_2020">
+            <label for="Fecha_2021">Fecha_2021</label>
+            <input type="text" class="form-control" id="Fecha_2021" name="Fecha_2021">
+            <label for="Fecha_2022">Fecha_2022</label>
+            <input type="text" class="form-control" id="Fecha_2022" name="Fecha_2022">
+            <label for="Fecha_2023">Fecha_2023</label>
+            <input type="text" class="form-control" id="Fecha_2023" name="Fecha_2023">
+            <label for="Fecha_2024">Fecha_2024</label>
+            <input type="text" class="form-control" id="Fecha_2024" name="Fecha_2024">
+            <label for="Fecha_2025">Fecha_2025</label>
+            <input type="text" class="form-control" id="Fecha_2025" name="Fecha_2025">
+            <label for="Fecha_2026">Fecha_2026</label>
+            <input type="text" class="form-control" id="Fecha_2026" name="Fecha_2026">
+            <label for="Fecha_2027">Fecha_2027</label>
+            <input type="text" class="form-control" id="Fecha_2027" name="Fecha_2027">
+            <label for="Fecha_2028">Fecha_2028</label>
+            <input type="text" class="form-control" id="Fecha_2028" name="Fecha_2028">
+            <label for="Fecha_2029">Fecha_2029</label>
+            <input type="text" class="form-control" id="Fecha_2029" name="Fecha_2029">
+            <label for="Fecha_2030">Fecha_2030</label>
+            <input type="text" class="form-control" id="Fecha_2030" name="Fecha_2030">
+            <label for="Feha_2021">Feha_2021</label>
+            <input type="text" class="form-control" id="Feha_2021" name="Feha_2021">
+
+
+
+
+
+           <button class="bt btn-primary mt-3">
+                      Agregar
+           </button>
+                      
+        </form>
+        </div>
+        </div>
+        </div>
+  </div>
+</div>
---
+++ ProyeccionHabitta/insertarproyeccionhabitta.php
@@ -0,0 +1,71 @@
+<?php 
+    include "./../clases/Conexion.php";
+    include "./../ProyeccionHabitta/crudproyeccionhabitta.php";
+
+    $crudporyeccionhabitta = new crudproyeccionhabitta();
+
+    $datos = array(
+        "MES" => $_POST['MES'], 
+        "fecha_2020" => $_POST['fecha_2020'],
+        "fecha_2021" => $_POST['fecha_2021'],
+        "fecha_2022" => $_POST['fecha_2022'],
+        "fecha_2023" => $_POST['fecha_2023'], 
+        "fecha_2024" => $_POST['fecha_2024'],
+        "fecha_2025" => $_POST['fecha_2025'],
+        "fecha_2026" => $_POST['fecha_2026'],
+        "fecha_2027" => $_POST['fecha_2027'], 
+        "fecha_2028" => $_POST['fecha_2028'],
+        "fecha_2029" => $_POST['fecha_2029'],
+        "fecha_2030" => $_POST['fecha_2030'],
+        "fecha_2031" => $_POST['fecha_2031'], 
+        "fecha_2032" => $_POST['fecha_2032'],
+        "fecha_2033" => $_POST['fecha_2033'],
+        "fecha_2024" => $_POST['fecha_2024'],
+        "fecha_2025" => $_POST['fecha_2025'],
+        "fecha_2026" => $_POST['fecha_2026'],
+        "fecha_2027" => $_POST['fecha_2027'],
+        "fecha_2028" => $_POST['fecha_2028'],
+        "fecha_2029" => $_POST['fecha_2029'],
+        "fecha_2030" => $_POST['fecha_2030'],
+        "fecha_2031" => $_POST['fecha_2031'],
+        "fecha_2032" => $_POST['fecha_2032'],
+        "fecha_2033" => $_POST['fecha_2033'],
+        "fecha_2034" => $_POST['fecha_2034'],
+        "fecha_2035" => $_POST['fecha_2035'],
+        "fecha_2036" => $_POST['fecha_2036'],
+        "fecha_2037" => $_POST['fecha_2037'],
+        "fecha_general" => $_POST['fecha_general'],
+        "DESIERTO" => $_POST['DESIERTO'],
+        "ESTEPA" => $_POST['ESTEPA'],
+        "PARAMO" => $_POST['PARAMO'],
+        "TAIGA" => $_POST['TAIGA'],
+        "BOSQUE" => $_POST['BOSQUE'],
+        "LOMAS" => $_POST['LOMAS'],
+        "Total_general" => $_POST['Total_general'],
+        "SELVA" => $_POST['SELVA'],
+        "LAGO" => $_POST['LAGO'],
+        "MANGLAR" => $_POST['MANGLAR'],
+        "Fecha_2020" => $_POST['Fecha_2020'],
+        "Fecha_2021" => $_POST['Fecha_2021'],
+        "Fecha_2022" => $_POST['Fecha_2022'],
+        "Fecha_2023" => $_POST['Fecha_2023'],
+        "Fecha_2024" => $_POST['Fecha_2024'],
+        "Fecha_2025" => $_POST['Fecha_2025'],
+        "Fecha_2026" => $_POST['Fecha_2026'],
+        "Fecha_2027" => $_POST['Fecha_2027'],
+        "Fecha_2028" => $_POST['Fecha_2028'],
+        "Fecha_2029" => $_POST['Fecha_2029'],
+        "Fecha_2030" => $_POST['Fecha_2030'],
+        "Feha_2021" => $_POST['Feha_2021'],
+
+
+
+
+
+
+
+
+        
+    );
+
+   $respuesta = $crudporyeccionhabitta->insertarDatos($datos);
