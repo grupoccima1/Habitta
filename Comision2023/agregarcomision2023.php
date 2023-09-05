72962a19673fe0d3b9307c5d8205d4c5abc7390d
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-04 18:06:26 -0600

    Avance 04/09/2023

---
+++ Comision2023/agregarcomision2023.php
@@ -0,0 +1,81 @@
+<div class="card">
+  <div class="card-body">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <div class= "card mt-4">
+    <div class= "card-body">
+        <h2> Agregar nuevo registro  </h2>
+        <form action="./insertarcomisiones2023.php" method="post">
+            <label for="LLAVE">LLAVE</label>
+            <input type="text" class="form-control" id="LLAVE" name="LLAVE">
+            <label for="FECHA_DE_INGRESO">FECHA DE INGRESO</label>
+            <input type="text" class="form-control" id="FECHA_DE_INGRESO" name="FECHA_DE_INGRESO">
+            <label for="LOTE">LOTE</label>
+            <input type="text" class="form-control" id="LOTE" name="LOTE">
+            <label for="CONDOMINIO">CONDOMINIO</label>
+            <input type="text" class="form-control" id="CONDOMINIO" name="CONDOMINIO">
+            <label for="CLUSTER">CLUSTER</label>
+            <input type="text" class="form-control" id="CLUSTER" name="CLUSTER">
+            <label for="DESARROLLO">DESARROLLO</label>
+            <input type="text" class="form-control" id="DESARROLLO" name="DESARROLLO">
+            <label for="PUESTO">PUESTO</label>
+            <input type="text" class="form-control" id="PUESTO" name="PUESTO">
+            <label for="COMISIONISTA">COMISIONISTA</label>
+            <input type="text" class="form-control" id="COMISIONISTA" name="COMISIONISTA">
+            <label for="TOTAL_DE_LA_VENTA">TOTAL DE LA VENTA</label>
+            <input type="text" class="form-control" id="TOTAL_DE_LA_VENTA" name="TOTAL_DE_LA_VENTA">
+            <label for="ENGANCHE">ENGANCHE</label>
+            <input type="text" class="form-control" id="ENGANCHE" name="ENGANCHE">
+            <label for="PCOMISION">PCOMISION</label>
+            <input type="text" class="form-control" id="PCOMISION" name="PCOMISION">
+            <label for="TOTAL_COMISION">TOTAL COMISION</label>
+            <input type="text" class="form-control" id="TOTAL_COMISION" name="TOTAL_COMISION">
+            <label for="DESCUENTO">DESCUENTO</label>
+            <input type="text" class="form-control" id="DESCUENTO" name="DESCUENTO">
+            <label for="A_PAGAR_EXTERNOS">A PAGAR EXTERNOS</label>
+            <input type="text" class="form-control" id="A_PAGAR_EXTERNOS" name="A_PAGAR_EXTERNOS">
+            <label for="SUBTOTAL">SUBTOTAL</label>
+            <input type="text" class="form-control" id="SUBTOTAL" name="SUBTOTAL">
+            <label for="IVA">IVA</label>
+            <input type="text" class="form-control" id="IVA" name="IVA">
+            <label for="RETENCIONES_DE_IVA">RETENCIONES DE IVA</label>
+            <input type="text" class="form-control" id="RETENCIONES_DE_IVA" name="RETENCIONES_DE_IVA">
+            <label for="RETENCIONES_DE_ISR">RETENCIONES DE ISR</label>
+            <input type="text" class="form-control" id="RETENCIONES_DE_ISR" name="RETENCIONES_DE_ISR">
+            <label for="TOTAL">TOTAL</label>
+            <input type="text" class="form-control" id="TOTAL" name="TOTAL">
+            <label for="PAGO">PAGO</label>
+            <input type="text" class="form-control" id="PAGO" name="PAGO">
+            <label for="PAGADO">PAGADO</label>
+            <input type="text" class="form-control" id="PAGADO" name="PAGADO">
+            <label for="METODO_DE_PAGO">METODO DE PAGO</label>
+            <input type="text" class="form-control" id="METODO_DE_PAGO" name="METODO_DE_PAGO">
+            <label for="FECHA_PAGADA">FECHA PAGADA</label>
+            <input type="text" class="form-control" id="FECHA_PAGADA" name="FECHA_PAGADA">
+            <label for="NOMBRE_CORRECTO">NOMBRE CORRECTO</label>
+            <input type="text" class="form-control" id="NOMBRE_CORRECTO" name="NOMBRE_CORRECTO">
+            <label for="SEMANA_PAGADA">SEMANA PAGADA</label>
+            <input type="text" class="form-control" id="SEMANA_PAGADA" name="SEMANA_PAGADA">
+            <label for="SEMANA">SEMANA</label>
+            <input type="text" class="form-control" id="SEMANA" name="SEMANA">
+            <label for="X_TIPO_DE_REGIMEN">X TIPO DE REGIMEN</label>
+            <input type="text" class="form-control" id="X_TIPO_DE_REGIMEN" name="X_TIPO_DE_REGIMEN">
+            <label for="CUENTA">CUENTA</label>
+            <input type="text" class="form-control" id="CUENTA" name="CUENTA">
+            <label for="MOTIVO_DE_DESCUENTO">MOTIVO DE DESCUENTO</label>
+            <input type="text" class="form-control" id="MOTIVO_DE_DESCUENTO" name="MOTIVO_DE_DESCUENTO">
+            <label for="ESTATUS">ESTATUS</label>
+            <input type="text" class="form-control" id="ESTATUS" name="ESTATUS">
+            <label for="LOTE_ANTERIOR">LOTE ANTERIOR</label>
+            <input type="text" class="form-control" id="LOTE_ANTERIOR" name="LOTE_ANTERIOR">
+           <button class="bt btn-primary mt-3">
+                      Agregar
+           </button>
+                      
+        </form>
+        </div>
+        </div>
+        </div>
+  </div>
+</div>
