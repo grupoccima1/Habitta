bfdec60d7a4288dcc38bde980fb0626517bd26e3
diegoDguezz <software.fe@grupoccima.com>
2023-09-04 18:20:43 -0600

    cambios a las vistas

--- Estados/agregarestados.php
+++ Estados/agregarestados.php
@@ -1,67 +1,110 @@
-<div class="card">
-  <div class="card-body">
-    <div class="container">
-    <div class="row">
-    <div class="col">
-    <div class= "card mt-4">
+<?php include "../header.php"; ?>
+<div class="container">
+<div class= "card mt-4">
     <div class= "card-body">
-        <h2> Agregar nuevo registro  </h2>
+        <h2 class="mb-3 fs-4 text-center"> Agregar nuevo registro  </h2>
         <form action="./insertarestados.php" method="post">
+          <div class="row">
+          <div class="col-4 mb-2">
             <label for="SourceName">SourceName</label>
-            <input type="text" class="form-control" id="SourceName" name="SourceName">
-            <label for="PERIODO">PERIODO</label>
-            <input type="text" class="form-control" id="PERIODO" name="PERIODO">
-            <label for="FECHA">FECHA</label>
-            <input type="text" class="form-control" id="FECHA" name="FECHA">
-            <label for="SALDOINICIAL">SALDOINICIAL</label>
-            <input type="text" class="form-control" id="SALDOINICIAL" name="SALDOINICIAL">
-            <label for="MENSUALIDAD">MENSUALIDAD</label>
-            <input type="text" class="form-control" id="MENSUALIDAD" name="MENSUALIDAD">
-            <label for="PAGADO">PAGADO</label>
-            <input type="text" class="form-control" id="PAGADO" name="PAGADO">
-            <label for="INTERESFINANCIAMIENTO">INTERESFINANCIAMIENTO</label>
-            <input type="text" class="form-control" id="INTERESFINANCIAMIENTO" name="INTERESFINANCIAMIENTO">
-            <label for="ABONO_CAPITAL">ABONO_CAPITAL</label>
-            <input type="text" class="form-control" id="ABONO_CAPITAL" name="ABONO_CAPITAL">
-            <label for="SALDO_FINAL">SALDO_FINAL</label>
-            <input type="text" class="form-control" id="SALDO_FINAL" name="SALDO_FINAL">
-            <label for="INT_GEN">INT_GEN</label>
-            <input type="text" class="form-control" id="INT_GEN" name="INT_GEN">
-            <label for="INT_INT">INT_INT</label>
-            <input type="text" class="form-control" id="INT_INT" name="INT_INT">
-            <label for="INT_ACUM_MORA">INT_ACUM_MORA</label>
-            <input type="text" class="form-control" id="INT_ACUM_MORA" name="INT_ACUM_MORA">
-            <label for="INT_NEG">INT NEG</label>
-            <input type="text" class="form-control" id="INT_NEG" name="INT_NEG">
-            <label for="ESTATUS">ESTATUS</label>
-            <input type="text" class="form-control" id="ESTATUS" name="ESTATUS">
-            <label for="OBSERVACION">OBSERVACION</label>
-            <input type="text" class="form-control" id="OBSERVACION" name="OBSERVACION">
-            <label for="DEBE_INTERES">DEBE_INTERES</label>
-            <input type="text" class="form-control" id="DEBE_INTERES" name="DEBE_INTERES">
-            <label for="FECHA_PAGO">FECHA_PAGO</label>
-            <input type="text" class="form-control" id="FECHA_PAGO" name="FECHA_PAGO">
-            <label for="INDICADOR_FECHA">INDICADOR_FECHA</label>
-            <input type="text" class="form-control" id="INDICADOR_FECHA" name="INDICADOR_FECHA">
-            <label for="PORCENT_INT">PORCENT_INT</label>
-            <input type="text" class="form-control" id="PORCENT_INT" name="PORCENT_INT">
-            <label for="LLAVE2">ELLAVE2</label>
-            <input type="text" class="form-control" id="LLAVE2" name="LLAVE2">
-            <label for="MENS_ADEU">SÍ NO</label>
-            <input type="text" class="form-control" id="MENS_ADEU" name="MENS_ADEU">
-            <label for="CALC_SOBRE">CALC_SOBRE</label>
-            <input type="text" class="form-control" id="CALC_SOBRE" name="CALC_SOBRE">
-            <label for="MONTO_INT_MOR">MONTO_INT_MOR</label>
-            <input type="text" class="form-control" id="MONTO_INT_MOR" name="MONTO_INT_MOR">
-           
-           
-           <button class="bt btn-primary mt-3">
-                      Agregar
-           </button>
-                      
+              <input type="text" class="form-control" id="SourceName" name="SourceName">
+          </div>  
+            <div class="col-4 mb-2">
+              <label for="PERIODO">PERIODO</label>
+              <input type="text" class="form-control" id="PERIODO" name="PERIODO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="FECHA">FECHA</label>
+              <input type="text" class="form-control" id="FECHA" name="FECHA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="SALDOINICIAL">SALDOINICIAL</label>
+              <input type="text" class="form-control" id="SALDOINICIAL" name="SALDOINICIAL">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="MENSUALIDAD">MENSUALIDAD</label>
+              <input type="text" class="form-control" id="MENSUALIDAD" name="MENSUALIDAD">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="PAGADO">PAGADO</label>
+              <input type="text" class="form-control" id="PAGADO" name="PAGADO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INTERESFINANCIAMIENTO">INTERESFINANCIAMIENTO</label>
+              <input type="text" class="form-control" id="INTERESFINANCIAMIENTO" name="INTERESFINANCIAMIENTO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="ABONO_CAPITAL">ABONO_CAPITAL</label>
+              <input type="text" class="form-control" id="ABONO_CAPITAL" name="ABONO_CAPITAL">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="SALDO_FINAL">SALDO_FINAL</label>
+              <input type="text" class="form-control" id="SALDO_FINAL" name="SALDO_FINAL">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INT_GEN">INT_GEN</label>
+              <input type="text" class="form-control" id="INT_GEN" name="INT_GEN">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INT_INT">INT_INT</label>
+              <input type="text" class="form-control" id="INT_INT" name="INT_INT">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INT_ACUM_MORA">INT_ACUM_MORA</label>
+              <input type="text" class="form-control" id="INT_ACUM_MORA" name="INT_ACUM_MORA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INT_NEG">INT NEG</label>
+              <input type="text" class="form-control" id="INT_NEG" name="INT_NEG">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="ESTATUS">ESTATUS</label>
+              <input type="text" class="form-control" id="ESTATUS" name="ESTATUS">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="OBSERVACION">OBSERVACION</label>
+              <input type="text" class="form-control" id="OBSERVACION" name="OBSERVACION">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="DEBE_INTERES">DEBE_INTERES</label>
+              <input type="text" class="form-control" id="DEBE_INTERES" name="DEBE_INTERES">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="FECHA_PAGO">FECHA_PAGO</label>
+              <input type="text" class="form-control" id="FECHA_PAGO" name="FECHA_PAGO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INDICADOR_FECHA">INDICADOR_FECHA</label>
+              <input type="text" class="form-control" id="INDICADOR_FECHA" name="INDICADOR_FECHA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="PORCENT_INT">PORCENT_INT</label>
+              <input type="text" class="form-control" id="PORCENT_INT" name="PORCENT_INT">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="LLAVE2">ELLAVE2</label>
+              <input type="text" class="form-control" id="LLAVE2" name="LLAVE2">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="MENS_ADEU">SÍ NO</label>
+              <input type="text" class="form-control" id="MENS_ADEU" name="MENS_ADEU">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="CALC_SOBRE">CALC_SOBRE</label>
+              <input type="text" class="form-control" id="CALC_SOBRE" name="CALC_SOBRE">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="MONTO_INT_MOR">MONTO_INT_MOR</label>
+              <input type="text" class="form-control" id="MONTO_INT_MOR" name="MONTO_INT_MOR">
+            </div>
+          </div>
+            <div class="d-flex justify-content-end">
+              <button class="btn btn-primary mt-3 px-5">
+                         Agregar
+              </button>
+                         
+            </div> 
         </form>
-        </div>
-        </div>
-        </div>
-  </div>
+    </div>
+    </div>
 </div>
--- HSBCHABITTA/agregarhsbchabitta.php
+++ HSBCHABITTA/agregarhsbchabitta.php
@@ -1,68 +1,114 @@
-<div class="card">
-  <div class="card-body">
-    <div class="container">
-    <div class="row">
-    <div class="col">
-    <div class= "card mt-4">
+<?php include "../header.php"; ?>
+<div class="container">
+<div class= "card mt-4">
     <div class= "card-body">
-        <h2> Agregar nuevo registro  </h2>
+        <h2 class="mb-3 fs-4 text-center"> Agregar nuevo registro  </h2>
         <form action="./insertarhsbchabitta.php" method="post">
+          <div class="row">
+          <div class="col-4 mb-2">
             <label for="SEMANA">SEMANA</label>
-            <input type="text" class="form-control" id="SEMANA" name="SEMANA">
-            <label for="FECHA">FECHA</label>
-            <input type="text" class="form-control" id="FECHA" name="FECHA">
-            <label for="BENEFICIARIO">BENEFICIARIO</label>
-            <input type="text" class="form-control" id="BENEFICIARIO" name="BENEFICIARIO">
-            <label for="INGRESO">INGRESO</label>
-            <input type="text" class="form-control" id="INGRESO" name="INGRESO">
-            <label for="EGRESO">EGRESO</label>
-            <input type="text" class="form-control" id="EGRESO" name="EGRESO">
-            <label for="SALDO">SALDO</label>
-            <input type="text" class="form-control" id="SALDO" name="SALDO">
-            <label for="FACTURA">FACTURA</label>
-            <input type="text" class="form-control" id="FACTURA" name="FACTURA">
-            <label for="REAL">REAL</label>
-            <input type="text" class="form-control" id="REAL" name="REAL">
-            <label for="MOTIVO">MOTIVO</label>
-            <input type="text" class="form-control" id="MOTIVO" name="MOTIVO">
-            <label for="MES">MES</label>
-            <input type="text" class="form-control" id="MES" name="MES">
-            <label for="DESARROLLO">DESARROLLO</label>
-            <input type="text" class="form-control" id="DESARROLLO" name="DESARROLLO">
-            <label for="LOTE">LOTE</label>
-            <input type="text" class="form-control" id="LOTE" name="LOTE">
-            <label for="CONDOMINIO">CONDOMINIO</label>
-            <input type="text" class="form-control" id="CONDOMINIO" name="CONDOMINIO">
-            <label for="CLUSTER">CLUSTER</label>
-            <input type="text" class="form-control" id="CLUSTER" name="CLUSTER">
-            <label for="OBRA">OBRA</label>
-            <input type="text" class="form-control" id="OBRA" name="OBRA">
-            <label for="CATEGORIA">CATEGORIA</label>
-            <input type="text" class="form-control" id="CATEGORIA" name="CATEGORIA">
-            <label for="SUBCATEGORIA">SUBCATEGORIA</label>
-            <input type="text" class="form-control" id="SUBCATEGORIA" name="SUBCATEGORIA">
-            <label for="UNIDADDENEGOCIO">UNIDADDENEGOCIO</label>
-            <input type="text" class="form-control" id="UNIDADDENEGOCIO" name="UNIDADDENEGOCIO">
-            <label for="FLUJO">FLUJO</label>
-            <input type="text" class="form-control" id="FLUJO" name="FLUJO">
-            <label for="LLAVE">LLAVE</label>
-            <input type="text" class="form-control" id="LLAVE" name="LLAVE">
-            <label for="FECHACORRECTA">FECHACORRECTAL</label>
-            <input type="text" class="form-control" id="FECHACORRECTA" name="FECHACORRECTA">
-            <label for="LLAVEEDOSDECUENTA">LLAVEEDOSDECUENTA</label>
-            <input type="text" class="form-control" id="LLAVEEDOSDECUENTA" name="LLAVEEDOSDECUENTA">
-            <label for="CLAVE">CLAVE</label>
-            <input type="text" class="form-control" id="CLAVE" name="CLAVE">
-            <label for="METODODEPAGO">METODODEPAGO</label>
-            <input type="text" class="form-control" id="METODODEPAGO" name="METODODEPAGO">
-
-           <button class="bt btn-primary mt-3">
-                      Agregar
-           </button>
+              <input type="text" class="form-control" id="SEMANA" name="SEMANA">
+          </div>  
+            <div class="col-4 mb-2">
+              <label for="FECHA">FECHA</label>
+              <input type="text" class="form-control" id="FECHA" name="FECHA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="BENEFICIARIO">BENEFICIARIO</label>
+              <input type="text" class="form-control" id="BENEFICIARIO" name="BENEFICIARIO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="INGRESO">INGRESO</label>
+              <input type="text" class="form-control" id="INGRESO" name="INGRESO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="EGRESO">EGRESO</label>
+              <input type="text" class="form-control" id="EGRESO" name="EGRESO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="SALDO">SALDO</label>
+              <input type="text" class="form-control" id="SALDO" name="SALDO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="FACTURA">FACTURA</label>
+              <input type="text" class="form-control" id="FACTURA" name="FACTURA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="REAL">REAL</label>
+              <input type="text" class="form-control" id="REAL" name="REAL">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="MOTIVO">MOTIVO</label>
+              <input type="text" class="form-control" id="MOTIVO" name="MOTIVO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="MES">MES</label>
+              <input type="text" class="form-control" id="MES" name="MES">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="DESARROLLO">DESARROLLO</label>
+              <input type="text" class="form-control" id="DESARROLLO" name="DESARROLLO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="LOTE">LOTE</label>
+              <input type="text" class="form-control" id="LOTE" name="LOTE">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="CONDOMINIO">CONDOMINIO</label>
+              <input type="text" class="form-control" id="CONDOMINIO" name="CONDOMINIO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="CLUSTER">CLUSTER</label>
+              <input type="text" class="form-control" id="CLUSTER" name="CLUSTER">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="OBRA">OBRA</label>
+              <input type="text" class="form-control" id="OBRA" name="OBRA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="CATEGORIA">CATEGORIA</label>
+              <input type="text" class="form-control" id="CATEGORIA" name="CATEGORIA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="SUBCATEGORIA">SUBCATEGORIA</label>
+              <input type="text" class="form-control" id="SUBCATEGORIA" name="SUBCATEGORIA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="UNIDADDENEGOCIO">UNIDADDENEGOCIO</label>
+              <input type="text" class="form-control" id="UNIDADDENEGOCIO" name="UNIDADDENEGOCIO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="FLUJO">FLUJO</label>
+              <input type="text" class="form-control" id="FLUJO" name="FLUJO">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="LLAVE">LLAVE</label>
+              <input type="text" class="form-control" id="LLAVE" name="LLAVE">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="FECHACORRECTA">FECHACORRECTAL</label>
+              <input type="text" class="form-control" id="FECHACORRECTA" name="FECHACORRECTA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="LLAVEEDOSDECUENTA">LLAVEEDOSDECUENTA</label>
+              <input type="text" class="form-control" id="LLAVEEDOSDECUENTA" name="LLAVEEDOSDECUENTA">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="CLAVE">CLAVE</label>
+              <input type="text" class="form-control" id="CLAVE" name="CLAVE">
+            </div>
+            <div class="col-4 mb-2">
+              <label for="METODODEPAGO">METODODEPAGO</label>
+              <input type="text" class="form-control" id="METODODEPAGO" name="METODODEPAGO">
+            </div>
+          </div>
+          <div class="d-flex justify-content-end">
+            <button class="btn btn-primary mt-3 px-5">
+                       Agregar
+            </button>
+          </div>
                       
         </form>
-        </div>
-        </div>
-        </div>
+    </div>
   </div>
 </div>
