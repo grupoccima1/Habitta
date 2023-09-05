32aae4d6683925299ad1b6a9e946e19940711a18
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-04 18:02:10 -0600

    Avance 04/09/2023

---
+++ Estados/agregarestados.php
@@ -0,0 +1,67 @@
+<div class="card">
+  <div class="card-body">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <div class= "card mt-4">
+    <div class= "card-body">
+        <h2> Agregar nuevo registro  </h2>
+        <form action="./insertarestados.php" method="post">
+            <label for="SourceName">SourceName</label>
+            <input type="text" class="form-control" id="SourceName" name="SourceName">
+            <label for="PERIODO">PERIODO</label>
+            <input type="text" class="form-control" id="PERIODO" name="PERIODO">
+            <label for="FECHA">FECHA</label>
+            <input type="text" class="form-control" id="FECHA" name="FECHA">
+            <label for="SALDOINICIAL">SALDOINICIAL</label>
+            <input type="text" class="form-control" id="SALDOINICIAL" name="SALDOINICIAL">
+            <label for="MENSUALIDAD">MENSUALIDAD</label>
+            <input type="text" class="form-control" id="MENSUALIDAD" name="MENSUALIDAD">
+            <label for="PAGADO">PAGADO</label>
+            <input type="text" class="form-control" id="PAGADO" name="PAGADO">
+            <label for="INTERESFINANCIAMIENTO">INTERESFINANCIAMIENTO</label>
+            <input type="text" class="form-control" id="INTERESFINANCIAMIENTO" name="INTERESFINANCIAMIENTO">
+            <label for="ABONO_CAPITAL">ABONO_CAPITAL</label>
+            <input type="text" class="form-control" id="ABONO_CAPITAL" name="ABONO_CAPITAL">
+            <label for="SALDO_FINAL">SALDO_FINAL</label>
+            <input type="text" class="form-control" id="SALDO_FINAL" name="SALDO_FINAL">
+            <label for="INT_GEN">INT_GEN</label>
+            <input type="text" class="form-control" id="INT_GEN" name="INT_GEN">
+            <label for="INT_INT">INT_INT</label>
+            <input type="text" class="form-control" id="INT_INT" name="INT_INT">
+            <label for="INT_ACUM_MORA">INT_ACUM_MORA</label>
+            <input type="text" class="form-control" id="INT_ACUM_MORA" name="INT_ACUM_MORA">
+            <label for="INT_NEG">INT NEG</label>
+            <input type="text" class="form-control" id="INT_NEG" name="INT_NEG">
+            <label for="ESTATUS">ESTATUS</label>
+            <input type="text" class="form-control" id="ESTATUS" name="ESTATUS">
+            <label for="OBSERVACION">OBSERVACION</label>
+            <input type="text" class="form-control" id="OBSERVACION" name="OBSERVACION">
+            <label for="DEBE_INTERES">DEBE_INTERES</label>
+            <input type="text" class="form-control" id="DEBE_INTERES" name="DEBE_INTERES">
+            <label for="FECHA_PAGO">FECHA_PAGO</label>
+            <input type="text" class="form-control" id="FECHA_PAGO" name="FECHA_PAGO">
+            <label for="INDICADOR_FECHA">INDICADOR_FECHA</label>
+            <input type="text" class="form-control" id="INDICADOR_FECHA" name="INDICADOR_FECHA">
+            <label for="PORCENT_INT">PORCENT_INT</label>
+            <input type="text" class="form-control" id="PORCENT_INT" name="PORCENT_INT">
+            <label for="LLAVE2">ELLAVE2</label>
+            <input type="text" class="form-control" id="LLAVE2" name="LLAVE2">
+            <label for="MENS_ADEU">SÍ NO</label>
+            <input type="text" class="form-control" id="MENS_ADEU" name="MENS_ADEU">
+            <label for="CALC_SOBRE">CALC_SOBRE</label>
+            <input type="text" class="form-control" id="CALC_SOBRE" name="CALC_SOBRE">
+            <label for="MONTO_INT_MOR">MONTO_INT_MOR</label>
+            <input type="text" class="form-control" id="MONTO_INT_MOR" name="MONTO_INT_MOR">
+           
+           
+           <button class="bt btn-primary mt-3">
+                      Agregar
+           </button>
+                      
+        </form>
+        </div>
+        </div>
+        </div>
+  </div>
+</div>
