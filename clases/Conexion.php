97b5c5f163d608dc3182d1655a28963281a5abce
diegoDguezz <software.fe@grupoccima.com>
2023-09-11 11:14:02 -0600

    agregar git Ingonore

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -1,6 +1,6 @@
 <?php 
 
-    require_once  $_SERVER['DOCUMENT_ROOT'] . "/naves/vendor/autoload.php";
+    require_once  $_SERVER['DOCUMENT_ROOT'] . "/habitta/vendor/autoload.php";
     
         class Conexion {
         public function conectar(){
@@ -30,5 +30,3 @@
 
         }
     }
-
-?>   
