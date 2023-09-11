b5f18c17677ee8af07d2af98f16e71f88d3d4d8e
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-11 16:17:48 -0600

    Avance 11/09/2023

---
+++ Catalogo/eliminarcatalogo.php
@@ -0,0 +1,69 @@
+<?php 
+   
+    include "../clases/Conexion.php";
+    include "./../Catalogo/crudcatalogo.php";
+
+    $crudcatalogo = new crudcatalogo();
+    $id = $_POST['id'];
+
+    $datos = $crudcatalogo -> obtenerDocumentoCatalogo($id);
+?>
+
+<div class="container">
+<div class="row">
+<div class="card mt-4">
+  <div class="card-body">
+            <div class="col">
+
+                <a href="../catalogo.php" class="btn btn-outline-info">
+                    Regresar
+                </a>
+
+                <h2>Eliminar  Registro</h2>
+                
+                <table class="table table-bordered">
+                    <thead>
+                    <th class="th__texto" style="text-align:center;">Llave</th>                        
+                        <th class="th__texto" style="text-align:center;">Porcentaje 1</th>
+                        <th class="th__texto" style="text-align:center;">Porcentaje 2</th>
+                        <th class="th__texto" style="text-align:center;">Porcentaje 3</th>
+                        
+                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td class="text-center"> <?php echo $datos->LLAVE; ?> </td>
+                          <td class="text-center"> <?php echo $datos->PORCENT1;?> </td>
+                          <td class="text-center"> <?php echo $datos->PORCENT2;?> </td>
+                          <td class="text-center"> <?php echo $datos->PORCENT3; ?> </td>
+                            </tr>
+                    </tbody>
+                </table>
+                <hr>
+                <div class="aler alert-danger" role="alert">
+                    <p>¿Estas seguro de eliminar este registro?</p>
+                    <p>
+                        Una vez eliminado no podras recuperarlo
+                    </p>
+                </div>
+                <form action="./eliminar_catalogo.php" method="POST">
+                    <input type="text" name="id" value="<?php echo $datos->_id;?>" hidden>
+                    <button class="btn btn-danger">
+                        Eliminar
+                    </button>                
+                </form>
+                <form action="" method="POST">
+                    <input type="text" name="id" value="<?php echo $datos->_id;?>" hidden>
+                    <button class="btn btn-warning">
+                        Modificar
+                    </button>                
+                </form>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php 
