ee22c965c50f6f18b8a085af465a316ec87c8229
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-14 18:24:59 -0600

    Avance 14/09/2023

---
+++ BBVA/actualizar_BBVA.php
@@ -0,0 +1,42 @@
+<?php
+    include "../clases/Conexion.php";
+    include "./../BBVA/crudbbva.php";
+
+    $crudbbva = new crudbbva();
+    $id = $_POST['id'];
+
+    $datos = array(
+        "SEMANA" => $_POST['SEMANA'], 
+        "FECHA" => $_POST['FECHA'],
+        "BENEFICIARIO" => $_POST['BENEFICIARIO'],
+        "INGRESO" => $_POST['INGRESO'],
+        "EGRESO" => $_POST['EGRESO'], 
+        "SALDO" => $_POST['SALDO'],
+        "FACTURA" => $_POST['FACTURA'],
+        "MOTIVO" => $_POST['MOTIVO'],
+        "MES" => $_POST['MES'], 
+        "DESARROLLO" => $_POST['DESARROLLO'],
+        "LOTE" => $_POST['LOTE'],
+        "CONDOMINIO" => $_POST['CONDOMINIO'],
+        "CLUSTER" => $_POST['CLUSTER'], 
+        "OBRA" => $_POST['OBRA'],
+        "CATEGORIA" => $_POST['CATEGORIA'],
+        "SUBCATEGORIA" => $_POST['SUBCATEGORIA'],
+        "MODELODENEGOCIO" => $_POST['MODELODENEGOCIO'],
+        "FLUJO" => $_POST['FLUJO'],
+        "LLAVE" => $_POST['LLAVE'],
+        "FECHACORRECTA" => $_POST['FECHACORRECTA'],
+        "LLAVEEDOSDECUENTA" => $_POST['LLAVEEDOSDECUENTA'],
+        "CLAVE" => $_POST['CLAVE'],
+        "METODODEPAGO" => $_POST['METODODEPAGO'],
+      
+    );
+
+    $respuesta = $crudbbva -> actualizarBBVA($id, $datos);
+
+    if ($respuesta->getModifiedCount() > 0 || $respuesta->getMatchedCount() > 0) {
+        header("location:../bbva.php");
+    }else{
+        print_r($respuesta);
+    }
+?>
