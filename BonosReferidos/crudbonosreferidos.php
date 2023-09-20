a74bc0ae7816116d0e6e19a67f8f3ad62c861c96
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:20:45 -0600

    cambios 19/09/2023

--- BonosReferidos/crudbonosreferidos.php
+++ BonosReferidos/crudbonosreferidos.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudbonosreferidos extends Conexion {
 
 
@@ -15,7 +15,48 @@ class crudbonosreferidos extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentoBonosReferidos($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->BonosReferidos;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarBonosReferidos($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->BonosReferidos;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatosBonosReferidos(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->BonosReferidos;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
 
       }
 ?>
