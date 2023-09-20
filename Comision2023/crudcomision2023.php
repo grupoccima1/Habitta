554257a16b3a33db5c3ea788ba5c0d0b884260a2
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:25:36 -0600

    cambios 19/09/2023

--- Comision2023/crudcomision2023.php
+++ Comision2023/crudcomision2023.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudcomision2023 extends Conexion {
 
 
@@ -15,7 +15,48 @@ class crudcomision2023 extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentoComision2023($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->comision2023;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarComision2023($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->comision2023;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatoscomisiones2023(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->comision2023;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
 
       }
 ?>
