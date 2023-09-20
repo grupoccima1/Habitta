dc6d46a23353baa31ee3de68869732ba5bc0d6ab
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:32:22 -0600

    cambios 19/09/2023

--- Listado/crudlistado.php
+++ Listado/crudlistado.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudlistado extends Conexion {
 
 
@@ -15,7 +15,63 @@ class crudlistado extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentoListado($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->listado;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarListado($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->listado;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatoslistados(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->listado;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
+    public function actualizarListado($id,$datos){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion -> listado;
+            $respuesta = $coleccion -> updateOne(
+                                                ['_id' => new MongoDB\BSON\ObjectId($id)],
+                                                [
+                                                    '$set' => $datos
+                                                ]
+                                                );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th -> getMessage();
+        }   
+    }
 
       }
 ?>
