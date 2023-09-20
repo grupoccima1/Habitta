29ba28ef7917a18d5baad8ca1d7f6c0197120306
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:35:57 -0600

    cambios 19/09/2023

--- billpocket/crudbillpocket.php
+++ billpocket/crudbillpocket.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudbillpocket extends Conexion {
 
 
@@ -15,7 +15,48 @@ class crudbillpocket extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentobillpocket($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->billpocket;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarbillpocket($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->billpocket;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatosbillpocket(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->billpocket;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
 
       }
 ?>
