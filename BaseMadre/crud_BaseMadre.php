e566edfacfb4a89b6b1fe2c7cbcd7b4f62ae9605
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:17:18 -0600

    cambios 19/09/2023

--- BaseMadre/crud_BaseMadre.php
+++ BaseMadre/crud_BaseMadre.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudbasemadre extends Conexion {
 
 
@@ -15,7 +15,48 @@ class crudbasemadre extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentoMadre($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->basemadre;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarMadre($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->basemadre;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatosmadre(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->basemadre;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
 
       }
 ?>
