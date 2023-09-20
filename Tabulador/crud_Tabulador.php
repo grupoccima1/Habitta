c34129b8003931e6d39519903c903d41fe919183
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:34:10 -0600

    cambios 19/09/2023

--- Tabulador/crud_Tabulador.php
+++ Tabulador/crud_Tabulador.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudtabulador extends Conexion {
 
 
@@ -15,6 +15,63 @@ class crudtabulador extends Conexion {
         } catch (\Throwable $th) {
             return $th -> getMessage();
         }
+    }
+    public function obtenerDocumentoTabulador($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->tabulador;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarTabulador($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->tabulador;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function mostrarDatostabulador(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->tabulador;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
+    public function actualizarTabulador($id,$datos){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion ->tabulador;
+            $respuesta = $coleccion -> updateOne(
+                                                ['_id' => new MongoDB\BSON\ObjectId($id)],
+                                                [
+                                                    '$set' => $datos
+                                                ]
+                                                );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th -> getMessage();
+        }   
     }
       }
 ?>
