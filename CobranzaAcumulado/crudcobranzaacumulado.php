8257f7fa86f433dbd096aedbc934f21dfcb85f96
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:23:50 -0600

    cambios 19/09/2023

--- CobranzaAcumulado/crudcobranzaacumulado.php
+++ CobranzaAcumulado/crudcobranzaacumulado.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudcobranzaacumulado extends Conexion {
 
 
@@ -16,6 +16,62 @@ class crudcobranzaacumulado extends Conexion {
         }
     }
 
+    public function obtenerDocumentoCobranzaAcumulado($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->cobranzaAcumulado;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarCobranzaAcumulado($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->cobranzaAcumulado;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarCobranzaAcumulado(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->cobranzaAcumulado;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
+    public function actualizarcobranzaacumulado($id,$datos){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion -> cobranzaAcumulado;
+            $respuesta = $coleccion -> updateOne(
+                                                ['_id' => new MongoDB\BSON\ObjectId($id)],
+                                                [
+                                                    '$set' => $datos
+                                                ]
+                                                );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th -> getMessage();
+        }   
+    }
       }
 ?>
