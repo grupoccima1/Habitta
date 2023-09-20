e34d3a338de0efbe916cfdd8aacadc7bcb8f90e7
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:30:01 -0600

    cambios 19/09/2023

--- HSBCHABITTA/crudhsbchabitta.php
+++ HSBCHABITTA/crudhsbchabitta.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+
 class crudhsbchabitta extends Conexion {
 
 
@@ -15,7 +15,63 @@ class crudhsbchabitta extends Conexion {
             return $th->getMessage();
         }
     }
+    public function obtenerDocumentoHSBCHABITTA($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->hsbc_habitta;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarHSBCHABITTA($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->hsbc_habitta;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 
+    public function mostrarDatoshsbc_habitta(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->hsbc_habitta;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    } 
+    public function actualizarHSBCHABITTA($id,$datos){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion -> hsbc_habitta;
+            $respuesta = $coleccion -> updateOne(
+                                                ['_id' => new MongoDB\BSON\ObjectId($id)],
+                                                [
+                                                    '$set' => $datos
+                                                ]
+                                                );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th -> getMessage();
+        }   
+    }
 
       }
 ?>
