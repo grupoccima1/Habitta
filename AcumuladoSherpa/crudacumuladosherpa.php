d8c1ed13cddcdbf0bd4e9e40a646de0012a19c84
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-09-19 18:14:59 -0600

    cambios 19/09/2023

--- AcumuladoSherpa/crudacumuladosherpa.php
+++ AcumuladoSherpa/crudacumuladosherpa.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once "./../clases/Conexion.php";
+//require_once "../clases/Conexion.php";
 class crudacumuladosherpa extends Conexion {
 
 
@@ -17,5 +17,47 @@ class crudacumuladosherpa extends Conexion {
     }
 
 
+    public function obtenerDocumentoSherpa($id) {
+        try {
+            if (!preg_match('/^[a-f0-9]{24}$/i', $id)) {
+                throw new Exception('El ID no tiene el formato correcto');
+            }
+            $conexion = parent::conectar();
+            $coleccion = $conexion->acumuladosherpa;
+            $datos = $coleccion->findOne(
+                                    array(
+                                        '_id' => new MongoDB\BSON\ObjectId($id)
+                                    )
+                                );
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function eliminarSherpa($id){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->acumuladosherpa;
+            $respuesta = $coleccion->deleteOne(
+                                            array(
+                                                "_id" => new MongoDB\BSON\ObjectId($id)
+                                            )   
+                                        );
+            return $respuesta;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function mostrarDatosAcumuladoSherpa(){
+        try {
+            $conexion = parent::conectar();
+            $coleccion = $conexion->acumuladosherpa;
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }  
       }
 ?>
