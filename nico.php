8bc7525b33276ae0a4a5864f635274ca5c18aaf0
JBARCENAS <ccimagithub@gmail.com>
2023-09-26 18:27:48 -0600

    Avance

--- nico.php
+++ nico.php
@@ -231,10 +231,6 @@ require_once "./clases/Conexion.php";
             </div>
             <div class="card-body">
               <div class="table-responsive">
-                <a href="./Nico/agregarnico.php" class="btn btn-primary">
-                  Agregar Registro
-                </a>
-                <hr>
                 <table id="tabla" class="table table-striped data-table" style="width: 100%">
                   <thead class="bg__td bg-blue__500 text-white" >
                     <tr>
@@ -260,8 +256,6 @@ require_once "./clases/Conexion.php";
                         <th class="th__texto" style="text-align:center;">CLIENTE</th>
                         <th class="th__texto" style="text-align:center;">CORREO</th>
                         <th class="th__texto" style="text-align:center;">TELEFONO</th>
-                        <th class="th__texto" style="text-align:center;">MODIFICAR</th>
-                        <th class="th__texto" style="text-align:center;">ELIMINAR</th>
 
                       
 
@@ -303,22 +297,6 @@ require_once "./clases/Conexion.php";
                           <td class="text-center"> <?php echo $item->CLIENTE;?> </td>
                           <td class="text-center"> <?php echo $item->CORREO;?> </td>
                           <td class="text-center"> <?php echo $item->TELEFONO;?> </td>
-                          <td class="text-center">
-                            <form action="./Nico/modificar_Nico.php" method="POST">
-                              <input type="text" name="id" id="id" hidden value="<?php echo $item->_id ?>">
-                              <button class="btn btn-warning">
-                                <i class="fa-solid fa-user-pen"></i>
-                              </button>
-                            </form>
-                          </td>
-                          <td class="text-center">
-                            <form action="./Nico/eliminarnico.php" method="POST">
-                              <input type="text" name="id" id="id" hidden value="<?php echo $item->_id ?>">
-                              <button class="btn btn-danger">
-                                <i class="fa-solid fa-user-pen"></i>
-                              </button>
-                            </form>
-                          </td>
                          
                          
                           </tr>
@@ -348,8 +326,6 @@ require_once "./clases/Conexion.php";
                         <th class="th__texto" style="text-align:center;">CLIENTE</th>
                         <th class="th__texto" style="text-align:center;">CORREO</th>
                         <th class="th__texto" style="text-align:center;">TELEFONO</th>
-                        <th class="th__texto" style="text-align:center;">MODIFICAR</th>
-                        <th class="th__texto" style="text-align:center;">ELIMINAR</th>
                     </tr>
                   </tfoot>
                 </table>
